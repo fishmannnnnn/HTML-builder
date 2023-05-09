fab4b4b20a2b743dd6da161e143eed55e989d3b8
Vlad <vlad.lemkov@yandex.ru>
2023-05-09 16:57:39 +0400

    fix: removed unnecessary imports

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,6 +1,4 @@
 const fs = require("fs");
-var os = require("os");
-const path = require("path");
 const readline = require("readline");
 const { stdin, stdout } = require("process");
 
@@ -9,8 +7,8 @@ console.log("Enter some text please:")
 const fileStream = fs.createWriteStream("output.txt", { flags: "a" });
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+    input: stdin,
+    output: stdout
 });
 
 rl.on("line", (input) => {
