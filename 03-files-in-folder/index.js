79ff3c746e8fc970115f46c760b096286c343665
Vlad <vlad.lemkov@yandex.ru>
2023-05-09 18:24:29 +0400

    feat: problem solved

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+
+const getFilesInfo = async () => {
+    const route = path.resolve(__dirname, "secret-folder");
+
+    const files = await fs.readdir(route, (e, files) => {
+        if (e) {
+            console.log(e)
+            return;
+        };
+        files.forEach((file => {
+            const filePath = path.resolve(route, file)
+            fs.stat(filePath, (e, stats) => {
+                if (e) {
+                    console.log(e)
+                    return;
+                }
+                
+                if (stats.isFile()) {
+                    console.log(`${file} - ${path.extname(filePath)} - ${stats.size} bytes`)
+                }
+            })
+        }))
+    });
+};
+
+getFilesInfo();
